79997eacd07ac3cf90769d1ab29432416e76797d
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-12-05 23:48:09 +0500

    Signup Ui

--- frontend/src/Pages/Signup/Signup.jsx
+++ frontend/src/Pages/Signup/Signup.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import './signup.css'
 
 const Signup = () => {
+
+    function changeHandler() {
+        console.log("change Handler working")
+    }
+
+    function loginHandler() {
+        console.log("Login handler working")
+    }
+
     return (
-        <div>Signup</div>
+        <>
+            <div className="login">
+                <div className="lContainer">
+                    <div className="l-input-group">
+                        <input type="email" required name="Email" className="l-input" id="Email" onChange={changeHandler} />
+                        <label className="l-user-label">Email</label>
+                    </div>
+                    <div className="l-input-group">
+                        <input type="text" required name="username" className="l-input" id="username" onChange={changeHandler} />
+                        <label className="l-user-label">Username</label>
+                    </div>
+                    <div className="l-input-group">
+                        <input type="password" required name="password" className="l-input" id="password" onChange={changeHandler} />
+                        <label className="l-user-label">Password</label>
+                    </div>
+                    <div className="l-input-group">
+                        <input type="password" required name="cPassword" className="l-input" id="cPassword" onChange={changeHandler} />
+                        <label className="l-user-label">Confirm Password</label>
+                    </div>
+                    <button className="l-button searchBtn" onClick={loginHandler}>Signup</button>
+                    {/* {isError && <span className="err">{isError.message}</span>} */}
+                </div>
+            </div>
+        </>
     )
 }
 
